64fffd751a1a88d087722931b383fd479c8fafad
FLegendre <F.Legendre@gmail.com>
2020-07-14 07:51:16 +0200

    Zip class for RAII

--- test.cpp
+++ test.cpp
@@ -3,21 +3,21 @@
 int
 main()
 {
-  // The namespace defines “table_t” as the table type (a std::vector of std::vector).
-  fd_write_xlsx::table_t table;
+	// The namespace defines “table_t” as the table type (a std::vector of std::vector).
+	fd_write_xlsx::table_t table;
 
-  // Populate the table with 3 rows...
-  {
-    using namespace fd_write_xlsx;
-    // The namespace defines “cell_t” as the cell type (a std::variant).
-    table.push_back({ cell_t{ "a" }, cell_t{ "b" }, cell_t{ "c" } });
-    // To insert a integer, you need an long integer.
-    table.push_back({ cell_t{ 1l }, cell_t{ 2l }, cell_t{ 3l } });
-    table.push_back({ cell_t{ 1.1 }, cell_t{ 2.2 }, cell_t{ 3.3 } });
-  }
+	// Populate the table with 3 rows...
+	{
+		using namespace fd_write_xlsx;
+		// The namespace defines “cell_t” as the cell type (a std::variant).
+		table.push_back({ cell_t{ "a" }, cell_t{ "b" }, cell_t{ "c" } });
+		// To insert a integer, you need an long integer.
+		table.push_back({ cell_t{ 1l }, cell_t{ 2l }, cell_t{ 3l } });
+		table.push_back({ cell_t{ 1.1 }, cell_t{ 2.2 }, cell_t{ 3.3 } });
+	}
 
-  // Then, write the table as a Microsoft xlsx workbook.
-  fd_write_xlsx::write("test.xlsx", table);
+	// Then, write the table as a Microsoft xlsx workbook.
+	fd_write_xlsx::write("test.xlsx", table);
 
-  return 0;
+	return 0;
 }
